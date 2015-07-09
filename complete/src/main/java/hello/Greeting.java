5f74369a1e6993bb63477e20ca05cda3dc33a91a
Ricatdo Luis Baumann <ricardo.luis.baumann@gmail.com>
2015-07-09 10:43:35 -0300

    tutorial dia 1

--- complete/src/main/java/hello/Greeting.java
+++ complete/src/main/java/hello/Greeting.java
@@ -2,15 +2,27 @@ package hello;
 
 public class Greeting {
 
-    private final long id;
-    private final String content;
+    private long id;
+    private String content;
 
     public Greeting(long id, String content) {
         this.id = id;
         this.content = content;
     }
 
-    public long getId() {
+    public Greeting() {
+		super();
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public long getId() {
         return id;
     }
 
--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -1,7 +1,13 @@
 package hello;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +16,38 @@ public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    private static Map<Long, Greeting> map = new HashMap<Long, Greeting>();
+    
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
+    
+    @RequestMapping(value="/greeting",method=RequestMethod.POST)
+    public Greeting post(@RequestBody Greeting greeting) {
+    	greeting.setId(counter.incrementAndGet());  
+    	map.put(greeting.getId(), greeting);
+    	return greeting;
+    }
+    
+    @RequestMapping(value="/greeting/{id}",method=RequestMethod.GET)
+    public Greeting get(@PathVariable("id") Long id ) {
+    	return map.get(id);
+    }
+    
+    @RequestMapping(value="/greeting/{id}",method=RequestMethod.PUT)
+    public Greeting get(@PathVariable("id") Long id, @RequestBody Greeting greeting) {
+    	greeting.setId(id);
+    	map.put(greeting.getId(), greeting);
+    	return greeting;
+    }
+    
+    @RequestMapping(value="/greeting/{id}",method=RequestMethod.DELETE)
+    public void delete(@PathVariable("id") Long id) {
+    	map.remove(id);
+    }
+    
+    
 }
